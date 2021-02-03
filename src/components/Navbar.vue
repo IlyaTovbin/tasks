efa0f4ac77a98e94af16ed553e5f9a3a634c118f
IlyaTovbin <ilyaxiv0@gmail.com>
2021-02-03 13:18:51 +0200

    Vue tasks

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="blue darken-3">
+    <div class="nav-wrapper ">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul class="right hide-on-med-and-down">
+        <router-link
+         to="/"
+         exact
+         active-class="active"
+         tag="li">
+            <a href="#">Create</a>
+        </router-link>
+        <router-link
+            to="/list"
+            exact
+            active-class="active"
+            tag="li">
+            <a href="#">List</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style lang="scss" scoped>
+    nav {
+        padding: 0 2rem;
+    }
+</style>
